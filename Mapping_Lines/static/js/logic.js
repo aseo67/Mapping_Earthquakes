6f9583f2f8bdb2f7529b8f81b08bf18ce508d4ee
aseo67 <85456812+aseo67@users.noreply.github.com>
2021-09-09 22:13:30 -0700

    Update files

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,56 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// ORIGINAL EXERCISE: CALI LINE PLOT
+// // Create the map object with center at the San Francisco airport.
+// let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// // Coordinates for each point to be used in the polyline.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790],
+//   [40.7899, -111.9791],
+//   [47.4502, -122.3088]
+// ];
+
+// // Create a polyline using the line coordinates and make the line black.
+// L.polyline(line, {
+//   color: "yellow"
+// }).addTo(map);
+
+// // We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// SKILL DRILL - SEA to SFO to AUS to YYZ to JFK
+// Create a map object
+let map = L.map('mapid').setView([40.7, -94.5], 5);
+
+// Coordinates for plot
+let line = [
+  [47.4436, -122.2961],
+  [37.6213, -122.3790],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
+]
+// Create polyline
+L.polyline(line, {
+  color: "blue",
+  weight: 4,
+  opacity: 0.5,
+  dashArray: 10
+}).addTo(map);
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
